b692ad63b8287dca9af9aa3c97cc2ae24d0d9249
LaureiVarju <mdilzell@yahoo.com>
2022-02-19 13:59:41 -0500

    changes to interactionCreate

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,8 +1,26 @@
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config.json');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const { readdirSync } = require('node:fs')
+const isJs = file => file.endsWith('.js')
+
 module.exports = {
 	name: 'interactionCreate',
 	execute(interaction) {
 		console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
-		interaction.reply("Hi from interaction create")
+	
+		interaction.client.commands = new Collection(readdirSync('./commands').filter(isJs).map(path => {
+		  const cmd = require(path)
+		  return [cmd.data.name, cmd]
+		}))
+		
+		readdirSync('./events').filter(isJs).forEach(path => {
+		  const { name, once, execute } = require(path)
+		  client[ once ? 'once' : 'on'](name, execute)
+		})
+		
+		client.login(token);
+		
 	},
 };
 

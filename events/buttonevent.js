3923fe338d40007f54c2a7278030cce9c16b5a00
LaureiVarju <mdilzell@yahoo.com>
2022-02-19 15:26:59 -0500

    adding button event

--- events/buttonevent.js
+++ events/buttonevent.js
@@ -3,13 +3,14 @@ module.exports = {
 	async execute(interaction) {
 		if (!interaction.isCommand()) return;
 
-		const command = interaction.client.commands.get(interaction.commandName);
+		const command = 'button'
 
 		if (!command) return;
 
 		try {
+            console.log("in the try block of button")
 			await command.execute(interaction);
-			console.log("in the try block of interactionCreate")
+			
 		} catch (error) {
 			console.error(error);
 			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });

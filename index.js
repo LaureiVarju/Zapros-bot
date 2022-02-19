c978bc5516077704c41d0c7b6dbcf912c580e13d
LaureiVarju <mdilzell@yahoo.com>
2022-02-19 14:04:47 -0500

    updates to index.js

--- index.js
+++ index.js
@@ -1,38 +1,21 @@
-const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
-	client.commands.set(command.data.name, command);
-}
+const { readdirSync } = require('node:fs')
 
+const isJs = file => file.endsWith('.js')
 
+interaction.client.commands = new Collection(readdirSync('./commands').filter(isJs).map(path => {
+  const cmd = require(path)
+  return [cmd.data.name, cmd]
+}))
 
-client.once('ready', () => {
-	console.log('Ready!');
-});
-
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
+readdirSync('./events').filter(isJs).forEach(path => {
+  const { name, once, execute } = require(path)
+  client[ once ? 'once' : 'on'](name, execute)
+})
 
 client.login(token);

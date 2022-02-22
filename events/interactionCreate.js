d47201a714607fa12d66b96e52906e3bf6140839
LaureiVarju <mdilzell@yahoo.com>
2022-02-22 11:24:20 -0500

    updated interactioncreate

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -6,24 +6,22 @@ module.exports = {
 			// select menu process
 		} else if (interaction.isCommand()) {
 			// slashie process
-
+		
 			const command = interaction.client.commands.get(interaction.commandName);
-
+		
 			if (!command) return;
-
+		
 			try {
-
+		
 				await command.execute(interaction);
 				console.log("in the try block of interactionCreate")
 			} catch (error) {
 				console.error(error);
 				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 			}
-
-
+		
+		
 		}
-
-
-	},
+	}
 };
 

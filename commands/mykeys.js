30b9de763a6ff05d7f3a64640b57f2f944e2ae3f
LaureiVarju <mdilzell@yahoo.com>
2022-03-05 19:01:50 -0500

    modularizing mykeys

--- commands/mykeys.js
+++ commands/mykeys.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-
+const helpers = require('../helpers');
+const createUserIdArray = helpers.createUserIdArray
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -8,50 +9,34 @@ module.exports = {
 	async execute(interaction) {
 
 		let userid = interaction.user.id
-
 		const fs = require('fs');
 		const rawdata = fs.readFileSync('../Zapros-bot/key_data.json');
 		const userdata = JSON.parse(rawdata);
-		const number_of_users = userdata.users.length
 		const discordId = (element) => element == interaction.user.id;
-
-
 
 		// myKeys functions as follows:
 		// 1. Using a helper method, it creates an array of users
 		// 2. It checks to see if the user exists in the database
 		// 3. If the user does exist, it generates a new array of just their key data
-
-
 		function myKeys() {
-			if (createUserArray().includes(interaction.user.id) == true) {
+			if (createUserIdArray(rawdata).includes(interaction.user.id) == true) {
 				console.log('you exist!')
-				let user_index = createUserArray().findIndex(discordId)
+				let user_index = createUserIdArray(rawdata).findIndex(discordId)
 				console.log('index value is  ' + userid)
 
 				//now make the key array for just this user
 				let character_array = []
-
-
-
 				for (let y = 0; y < userdata.users[user_index].characters.length; y++) {
-					console.log('value of y is ' + y)
-
-					//   row_data = JSON.stringify(userdata.users[user_index].characters[y].weekly_key + ' ' + userdata.users[user_index].characters[y].name + ' ' + userdata.users[user_index].characters[y].class)
+								
 					row_data = JSON.stringify(userdata.users[user_index].characters[y].weekly_key + ' ' + '[' + userdata.users[user_index].characters[y].key_level + ']' + ' ' + userdata.users[user_index].characters[y].character_name + '-' + userdata.users[user_index].characters[y].realm + ' ' + '(' + userdata.users[user_index].characters[y].character_class + ')')
 					cleaned_string = row_data.replace(/"/g, '')
 					character_array.push(cleaned_string)
 
-
 					//if we reach this part of the loop, we're done
 					if (y == userdata.users[user_index].characters.length - 1) {
-
 						return character_array
-
 					}
 				}
-
-				/// end of array logic
 
 			} else {
 				const NOT_IN_DATABASE = `I have no record of you, ${interaction.user.username}! Try using '/addcharacter' to get started!`
@@ -61,27 +46,7 @@ module.exports = {
 				return character_array
 			}
 		}
-
-		//helper functions
-		function createUserArray() {
-			let user_array = []
-			//outer loop i is set by our overall user level
-			for (let i = 0; i < number_of_users; i++) {
-				row_data = JSON.stringify(userdata.users[i].discordid)
-				cleaned_string = row_data.replace(/"/g, '')
-
-
-				user_array.push(cleaned_string)
-				//if we reach this part of the loop, we're done
-				if (i == number_of_users - 1) {
-					console.log("i == number of users - 1")
-					console.log(user_array)
-					return user_array
-				}
-			}
-		}
-
-
+	
 		if (myKeys()[0] != `I have no record of you, ${interaction.user.username}! Try using '/addcharacter' to get started!` ){
 		return interaction.reply({content: myKeys().join('\n') ,ephemeral: true})
 		} else return interaction.reply({content: `I have no record of you, ${interaction.user.username}! Try using '/addcharacter' to get started!`, ephemeral: true})

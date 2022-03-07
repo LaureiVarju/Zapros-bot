f73b556599f59eafdfd423541ec4b30977f53e7c
LaureiVarju <mdilzell@yahoo.com>
2022-03-07 10:23:35 -0500

    cleanup

--- commands/help.js
+++ commands/help.js
@@ -6,15 +6,16 @@ const { MessageEmbed } = require('discord.js');
 const exampleEmbed = new MessageEmbed()
 	.setColor('#0099ff')
 	.setTitle("Hi, I'm Zapros!")
-	.setDescription("I'm a bot that lets you and your friends keep track of your mythic + keys!\n\nVersion: 1.0.0-beta")
+	.setDescription("I'm a bot that lets you and your friends keep track of your mythic + keys")
 	.setThumbnail('https://i.pinimg.com/originals/0c/67/5a/0c675a8e1061478d2b7b21b330093444.gif')
 	.addFields(
-		{ name: 'Try these commands!', value: '\u200B' },
-		{ name: '/addcharacter', value: 'Adds a new character to my records' },
-		{ name: '/update', value: 'Lets you update your key data for any character' },
-		{ name: '/keys', value: 'Reports all up-to-date key data I have for this week' },
-		{ name: '/mykeys', value: 'Reports only YOUR up-to-date key data for the week' },
-		{ name: '/delete', value: 'Allows you to delete any or all of your own characters' }
+		{ name: '__Try these commands!__', value: '\u200B' },
+		{ name: '```/addcharacter```', value: 'Adds a new character to Zapros' },
+		{ name: '```/update```', value: 'Update your key data for any character' },
+		{ name: '```/keys```', value: 'Reports all up-to-date key data Zapros has for this week' },
+		{ name: '```/mykeys```', value: 'Reports your up-to-date key data for the week' },
+		{ name: '```/delete```', value: 'Allows you to delete any or all of your own characters' },
+		{ name: '```/joke```', value: 'Tells you a really, really, corny joke' }
 	
 	)
 	// .addField('Inline field title', 'Some value here', true)
--- commands/keys.js
+++ commands/keys.js
@@ -30,7 +30,7 @@ async function getuserdata(){
 const axios = require('axios')
 const APIpaths = require('../APIpaths');
 const periodAPI = APIpaths.periodAPI
-const affixAPI = presets.affixAPI
+const affixAPI = APIpaths.affixAPI
 const us_region = 0
 
 

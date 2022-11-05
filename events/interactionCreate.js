fe3d302732d63e807b118da862cc4779fea3d1b4
LaureiVarju <mdilzell@yahoo.com>
2022-11-04 21:35:41 -0400

    misc

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,10 +1,11 @@
+const moment = require('moment')
 module.exports = {
 	name: 'interactionCreate',
 	async execute(interaction) {
 
 		//handling a menu selection
 		if (interaction.isSelectMenu()) {
-			console.log(`menu customId event: "${interaction.customId}" from user: ${interaction.user.username}`)
+			console.log(`menu customId event: "${interaction.customId}" from user: ${interaction.user.username} at ${moment().format('MMMM Do YYYY, h:mm:ss a')}`)
 			const command = interaction.client.selectMenus.get(interaction.customId); //defined in index.js
 			if (!command) return;
 			try {
@@ -18,7 +19,7 @@ module.exports = {
 
 		// handling a button press
 		else if (interaction.isButton()) {
-			console.log(`button customId event: "${interaction.customId}" from user: ${interaction.user.username}`)
+			console.log(`button customId event: "${interaction.customId}" from user: ${interaction.user.username} at ${moment().format('MMMM Do YYYY, h:mm:ss a')}`)
 			const command = interaction.client.button.get(interaction.customId); //defined in index.js
 
 			if (!command) return;
@@ -36,7 +37,7 @@ module.exports = {
 		// used to suppress log messages where commandName is undefined, but can't be evaluated without causing an error
 		let test_var = interaction.commandName
 		if(test_var != undefined){
-		console.log(`commandName event: "${test_var}" from user: ${interaction.user.username}`)
+		console.log(`commandName event: "${test_var}" from user: ${interaction.user.username} at ${moment().format('MMMM Do YYYY, h:mm:ss a')}`)
 		}
 		
 		const command = interaction.client.commands.get(interaction.commandName); //defined in index.js
